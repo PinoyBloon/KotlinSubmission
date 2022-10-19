56c07578eef01621fb45613bb4b78417f0f1fa6f
PinoyBloon <ikhlazoelzephyr@gmail.com>
2022-10-19 18:54:44 +0700

    Initial commit

---
+++ src/main/kotlin/id/infinitelearning/KotlinSubmission/exercise3/App.kt
@@ -0,0 +1,44 @@
+package id.infinitelearning.KotlinSubmission.exercise3
+
+fun main() {
+
+    /** Latihan 1
+    Buatlah logika if untuk mengevaluasi nilai score dengan ketentuan:
+    1. Jika nilai score lebih atau sama dengan 90
+    - Isi variabel result dengan nilai: 'Selamat! Anda mendapatkan nilai A.'
+    2. Jika nilai score ada di antara 80 hingga 89
+    - Isi variabel result dengan nilai: 'Anda mendapatkan nilai B.'
+    3. Jika nilai score ada di antara 70 hingga 79
+    - Isi variabel result dengan nilai: 'Anda mendapatkan nilai C.'
+    4. Jika nilai score ada di antara 60 hingga 69:
+    - Isi variabel result dengan nilai: 'Anda mendapatkan nilai D.'
+    5. Jika nilai score di bawah 60:
+    - Isi variabel result dengan nilai: 'Anda mendapatkan nilai E.'
+     */
+    // Buat di bawah sini
+
+    val nilaiA = 90
+    val nilaiB = 80
+    val nilaiC = 70
+    val nilaiD = 60
+
+    println("Enter Number: ")
+    val nilai = readln()
+
+    val nilaiAkhir = if (nilai >= nilaiA.toString()) {
+        "Selamat! Anda mendapatkan nilai A."
+    } else if (nilai >= nilaiB.toString()) {
+        "Anda mendapatkan nilai B."
+    } else if (nilai >= nilaiC.toString()) {
+        "Anda mendapatkan nilai C."
+    } else if (nilai >= nilaiD.toString()) {
+        "Anda mendapatkan nilai D."
+    } else {
+        "Anda mendapatkan nilai E. GAGAL. NGULANG SONO!"
+    }
+
+    println(nilaiAkhir)
+
+
+
+}       /** MADE BY RAKHSASHA */
