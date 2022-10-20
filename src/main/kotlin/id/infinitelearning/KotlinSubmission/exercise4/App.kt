d02af8685f84e7ed822cb250f3bba159f0c9be66
PinoyBloon <ikhlazoelzephyr@gmail.com>
2022-10-20 14:58:01 +0700

    Finished

--- src/main/kotlin/id/infinitelearning/KotlinSubmission/exercise4/App.kt
+++ src/main/kotlin/id/infinitelearning/KotlinSubmission/exercise4/App.kt
@@ -6,7 +6,14 @@ fun main() {
      * Buatlah contoh code penggunaan try catch untuk menangani suatu kasus sesuai keinginan teman-teman
      *
      */
-
-
-
+    try {
+        val honey = 2050
+        val money = "Gaada duit"
+        money.toByte()
+        honey.toString(12)
+    } catch (e:Exception) {
+        e.printStackTrace()
+    } finally {
+        println("Exception handling")
+    }
 }       /** MADE BY RAKHSASHA */
